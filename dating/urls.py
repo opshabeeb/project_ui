5450100b1be8e6a14e7c7994d189cfe2b242cb8e
opshabeeb <mohammedshabeeb0000@gmail.com>
2024-08-21 19:00:46 +0530

    msg

--- dating/urls.py
+++ dating/urls.py
@@ -4,7 +4,23 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-                  path('', views.TestView, name='test'),
+    path('', views.TestView, name='test'),
+    #accounts
+    path('first',views.FirstView.as_view(),name='first'),
+    path('login/',views.LoginView.as_view(),name='login'),
+    path('signup',views.SignupView.as_view(),name='signup'),
+    path('profession',views.ProfessionView.as_view(),name='profession'),
+    
+    
+    #groups
+    path('create_group',views.CreateGroupView.as_view(),name='create_group'),
+    path('groupslist',views.GroupListView.as_view(),name='group_list'),
+    
+    #payment
+    path('subscriptionplan/', views.SubscriptionPlanView.as_view(), name='subscription_plan'),
+    path('paymentmethods/', views.PaymentMethodsView.as_view(), name='payment_methods'),
+    path('addpaymentmethods/', views.AddPaymentMethodsView.as_view(), name='add_payment_methods'),
+    
               ]
 
 
--- dating/views.py
+++ dating/views.py
@@ -1,7 +1,50 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 
 # Create your views here.
 
 def TestView(request):
     return render(request, 'index.html')
+class FirstView(TemplateView):
+    template_name='account/first.html'
+
+class SignupView(TemplateView):
+    template_name='account/signup.html'
+
+
+class LoginView(TemplateView):
+    template_name='account/login.html'
+
+
+class ProfessionView(TemplateView):
+    template_name='account/profession.html'
+
+
+class Rel_GoalView(TemplateView):
+    template_name='account/relationship_goal.html'
+
+
+class InterestView(TemplateView):
+    template_name='account/interested.html'
+    
+
+# Group 
+class CreateGroupView(TemplateView):
+    template_name='groups/create_group.html'
+
+
+class GroupListView(TemplateView):
+    template_name = 'groups/groups.html'
+
+
+# SubscriptionPlan
+class SubscriptionPlanView(TemplateView):
+    template_name = 'payment/subscription_plans.html'
+
+
+class PaymentMethodsView(TemplateView):
+    template_name = 'payment/payment_methods.html'
+
+
+class AddPaymentMethodsView(TemplateView):
+    template_name = 'payment/add_payment.html'
